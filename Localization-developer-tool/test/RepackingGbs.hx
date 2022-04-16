340e9c7ad77c7074352bfe69cdb32bc64cb98d65
Devyatyi9 <devyatyi9@gmail.com>
2022-04-16 15:08:06 +0400

    update repacking gbs code

--- Localization-developer-tool/test/RepackingGbs.hx
+++ Localization-developer-tool/test/RepackingGbs.hx
@@ -5,7 +5,6 @@ import format.gbs_otterui.GbsData;
 import format.gbs_otterui.GbsReader;
 import format.gbs_otterui.GbsWriter;
 import format.gbs_otterui.Tools;
-// import format.wav.Reader;
 import haxe.io.Bytes;
 
 using format.gbs_otterui.Tools;
@@ -16,20 +15,24 @@ class RepackingGbs {
 
 	public function processing() {
 		// Cross platform paths
-		var eLocation = "otterui-project/Import/Import-pixel-ui/BuckLobby.gbs";
-		var ePath = new haxe.io.Path(eLocation);
-		trace(ePath.dir); // path/to
-		trace(ePath.file); // file
-		trace(ePath.ext); // txt
+		// var eLocation = "otterui-project/Import/Import-pixel-ui/BuckLobby.gbs";
+		// var ePath = new haxe.io.Path(eLocation);
+		// trace(ePath.dir); // path/to
+		// trace(ePath.file); // file
+		// trace(ePath.ext); // txt
 
 		// var location = "test/BuckLobby.gbs";
 		// var location = "test/Transitions.gbs";
 		// var location = "test/my_file.txt";
-		var location = "test/ImageGlyph-test.gbs";
+		// var location = "test/ImageGlyph-test.gbs";
 		// var location = "otterui-project/Import/Import-pixel-ui/BuckLobby.gbs";
 		// var location = "otterui-project/OtterExport/Export-pixel-ui/Scenes_pixel.gbs";
 
 		// gbsTestReadWrite(location);
+
+		#if debug
+		trace("Repacking gbs debug");
+		#end
 
 		//**PIXEL**/
 
@@ -42,7 +45,7 @@ class RepackingGbs {
 		var fileList_import_pixel = recursiveDir("otterui-project/Import/Import-pixel-ui");
 		//
 		// путь до Merged - pixel-ui
-		var path_merged_pixel = "otterui-project/Merged/pixel-ui-merged";
+		var path_merged_pixel = "otterui-project/Merged/pixel-ui-merged/";
 		//
 
 		// здесь проверяем массив сцен на наличие шрифтов
@@ -55,31 +58,15 @@ class RepackingGbs {
 		var objectList_export_pixel = readGbsList(fileList_export_pixel);
 		fileList_export_pixel = [];
 
-		trace(objectList_import_pixel[0].header.sceneID);
-		trace(objectList_export_pixel[0].header.sceneID);
-
-		#if debug
-		trace("debug");
-		#end
-
-		// objectList_export_pixel - получаем отсюда (строковый массив) список названий шрифтов
-		// objectList_import_pixel
-		// после добавления символов в шрифт, увеличивать длину шрифта на количество добавленных шрифтов
-		// 1 структура символа 40 байт
-
-		// var fNames_export_main = getFontNames(objectList_export_main);
-		// function (object_export:Array<GbsFile>, object_import:Array<GbsFile>)
-
-		// saveAsJson(objectList_export_pixel);
-
-		// mergeCycle(objectList_export_pixel, objectList_import_pixel);
-
 		var translatedFonts = fontsAllocate(objectList_export_pixel);
 		objectList_export_pixel = [];
-
 		var fromGameFonts = fontsComparingAllocate(translatedFonts, objectList_import_pixel);
+		// atlases_export_pixel
+
+		mergeFonts(fromGameFonts, translatedFonts);
+		trace(fileList_import_pixel[0]);
+
 		trace('test');
-		mergeFonts(fromGameFonts, translatedFonts, atlases_export_pixel);
 
 		//**MAIN**/
 
@@ -92,7 +79,7 @@ class RepackingGbs {
 		var fileList_import_main = recursiveDir("otterui-project/Import/Import-main-ui");
 		//
 		// путь до Merged - main-ui
-		var path_merged_main = "otterui-project/Merged/main-ui-merged";
+		var path_merged_main = "otterui-project/Merged/main-ui-merged/";
 		//
 
 		// здесь проверяем массив сцен на наличие шрифтов
@@ -104,13 +91,13 @@ class RepackingGbs {
 
 		var objectList_export_main = readGbsList(fileList_export_main);
 
-		trace(objectList_import_main[0].header.sceneID);
-		trace(objectList_export_main[0].header.sceneID);
+		// trace(objectList_import_main[0].header.sceneID);
+		// trace(objectList_export_main[0].header.sceneID);
 	}
 
-	function mergeFonts(importMap:Map<Int, GbsFont>, exportMap:Map<Int, GbsFont>, atlases_export:String) {
+	function mergeFonts(importMap:Map<Int, GbsFont>, exportMap:Map<Int, GbsFont>) {
 		for (key in importMap.keys()) {
-			trace('ключ ${key}');
+			trace('merging font key: ${key}');
 			var exportVal = exportMap[key];
 			var importVal = importMap[key];
 
@@ -129,7 +116,7 @@ class RepackingGbs {
 					// после все уникальные символы добавляются в конец массива fromGame
 					#if utf16
 					if ((charCodeExp == charCodeImp) || (charCodeExp == '?')) { // '?'
-					#elseif !utf16
+					#else
 					if ((charCodeExp == charCodeImp) || (charCodeExp == '?\x00')) { // '?\x00'
 					#end
 						var charExpObj = exportVal.charsBlock[nCharE];
@@ -145,7 +132,7 @@ class RepackingGbs {
 				// trace('character translated: ${charCodeExp}');
 				nCharE++;
 			}
-			trace('characters has been filtrated');
+			trace('characters in map has been filtrated');
 			// скорректировать число атласов и индексы атласов у символов
 			// здесь добавляем символы из exported в imported
 			var impCount = importVal.charsCount - 1;
@@ -160,10 +147,21 @@ class RepackingGbs {
 				nCharE++;
 			}
 			// не забыть переименовать png файлы шрифтов под новое значение индексов
+			// по количеству атласов в импорте и экспорте (импорт + экспорт + 1)
 			var expIndex = exportVal.charsBlock[0].charAtlasIndex;
-			trace(expIndex);
-			trace('nCharE: ${nCharE}');
-			// atlases_export;
+			// trace(expIndex);
+			// trace('nCharE: ${nCharE}');
+			var atlasSum = exportVal.atlasCount + importVal.atlasCount;
+			importVal.atlasCount = atlasSum;
+			if (exportVal.maxTop > importVal.maxTop)
+				importVal.maxTop = exportVal.maxTop;
+			for (char in exportVal.charsBlock) {
+				importVal.charsBlock.push(char);
+			}
+			exportVal.charsBlock = [];
+			importVal.charsCount = importVal.charsBlock.length;
+			importVal.fontLength = importVal.charsCount * 40 + 100;
+			// trace(importVal.fontLength);
 		}
 
 		// сравнение по MaxTop
@@ -172,6 +170,9 @@ class RepackingGbs {
 		// сравнение по количеству символов
 		// сравнение символов по индексам
 		// пересчитать длину шрифта
+		// после добавления символов в шрифт, увеличивать длину шрифта на количество добавленных шрифтов
+		// 1 структура символа 40 байт
+		// заголовок шрифта 100 байт
 	}
 
 	function fontsAllocate(o:Array<GbsFile>) {
@@ -251,6 +252,8 @@ function readGbsList(array:Array<String>) {
 	while (i < array.length) {
 		var o = readGuiFile(array[i]);
 		tmp.push(o);
+		var path = haxe.io.Path.withoutDirectory(array[i]);
+		array[i] = path;
 		i++;
 	}
 	return tmp;
@@ -276,6 +279,9 @@ function recursiveDir(directory:String) {
 				if (fileExt.ext == "gbs" && Tools.fileExists(path) == true) {
 					// trace('Gbs file found: $file');
 					paths.push(path);
+				}
+				if (fileExt.ext == "png") {
+					// здесь будет переименовывание файлов по индексам
 				}
 			}
 		}
